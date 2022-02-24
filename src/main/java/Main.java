550d95e7b36baed7f2a83a86b9907bc005c77054
Wei <wei@Zipcoders-MacBook-Pro.local>
2022-02-24 17:36:19 -0500

    finished lab

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,6 +6,18 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args ){
-    Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
+        Main names = new Main();
+        System.out.println("Enter your first name:");
+        String name = scan.nextLine(); //get name from input
+        names.AliceAndBob(name);
+        scan.close();
+    }
+    public void AliceAndBob(String name){
+        //checks if their name is bob or alice
+        if (name.equalsIgnoreCase("bob"))
+            System.out.println("Greetings "+name+".");
+        else if (name.equalsIgnoreCase("alice"))
+            System.out.println("Greetings "+name+".");
     }
 }
