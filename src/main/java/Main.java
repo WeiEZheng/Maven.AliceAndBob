f8390a4f1e4cae9457667c8c37136d1173a12544
Wei <wei@Zipcoders-MacBook-Pro.local>
2022-02-24 15:02:21 -0500

    created scanner

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,11 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
     public static void main(String[] args ){
-    //testing keys to github
+    Scanner scan = new Scanner(System.in);
     }
 }
